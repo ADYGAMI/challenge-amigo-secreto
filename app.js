42b600c8e83de6de4990f58050629a09ebfa5494
adygami <ady.jamg@gmail.com>
2025-02-23 20:32:26 -0600

    Se agrego archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;    
+    
+    if (amigo === '') {
+        alert('Por favor, inserte un nombre.');
+    }else{
+        amigos.push(amigo);
+        console.log(amigos);
+        document.querySelector('#amigo').value = '';
+        actualizarListaAmigos();
+    }
+    return;
+}
+
+function actualizarListaAmigos(){
+
+    // Seleccionar la lista por su ID
+    const listaAmigos = document.getElementById('listaAmigos');
+
+    // Vaciar el contenido de la lista
+    listaAmigos.innerHTML = '';
+
+    // Recorrer el array y agregar cada amigo a la lista
+    for (let i = 0; i < amigos.length; i++) {
+        const nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = amigos[i];
+        listaAmigos.appendChild(nuevoElemento);
+    }
+    return;
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        alert('Por favor, agregue al menos un amigo.');
+    }else{    
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.getElementById('resultado').textContent = `El amigo secreto sorteado es: ${amigoSorteado}`;
+        const listaAmigos = document.getElementById('listaAmigos');
+        listaAmigos.innerHTML = '';
+    }    
+    return;
+}
